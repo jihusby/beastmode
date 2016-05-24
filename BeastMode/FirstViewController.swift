82e2605420649ca67a2debc7bc1f42eee8e71a63
jihusby <jihusby@gmail.com>
2016-05-24 20:33:18 +0200

    Initial Commit

---
+++ BeastMode/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BeastMode
+//
+//  Created by Jan Inge Husby on 24.05.2016.
+//  Copyright © 2016 Jan Inge Husby. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
